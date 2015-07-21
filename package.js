f63ca1bc931d9feb0b9bc09f58a20debae5c9e80
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-07-21 21:09:21 +0200

    v0.2.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Reactive map for Meteor',
-  version: '0.2.1',
+  version: '0.2.2',
   name: 'jagi:reactive-map',
   git: 'https://github.com/jagi/meteor-reactive-map.git'
 });
@@ -9,6 +9,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
 
   api.use('underscore');
+  api.use('tracker');
 
   api.addFiles('lib/reactive_map.js', ['client', 'server']);
 
