18dafaa086b9759e27985c583400141638daaea3
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-09-27 21:06:59 +0200

    Bump version to 1.0.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Reactive map for Meteor',
-  version: '0.2.3',
+  version: '1.0.0',
   name: 'jagi:reactive-map',
   git: 'https://github.com/jagi/meteor-reactive-map.git'
 });
